8555cd79d175f22b26d3c7649fc49a52d149456e
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-09-24 08:32:14 +0200

    chore: trying to add a comment

--- src/action.js
+++ src/action.js
@@ -1,5 +1,21 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
 async function run() {
   console.log("Holla!");
+
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { issue } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: issue.number,
+    body: "I am so in ACTION!",
+  });
 }
 
 run();
