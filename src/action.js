15660e2a830a1052195d3d11b6d621321d590023
Vladan Paunovic <vladan.paunovic.bg@gmail.com>
2021-09-24 09:12:19 +0200

    chore: add jira

--- src/action.js
+++ src/action.js
@@ -2,9 +2,20 @@
 // import github from "@actions/github";
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Version3Client } = require("jira.js");
+
+const jiraClient = new Version3Client({
+  host: core.getInput("JIRA_API_HOST"),
+  authentication: {
+    basic: {
+      apiToken: core.getInput("JIRA_API_TOKEN"),
+      email: core.getInput("JIRA_EMAIL"),
+    },
+  },
+});
 
 async function run() {
-  console.log("Holla!");
+  const jiraProjectId = core.getInput("JIRA_PROJECT_ID");
 
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 
@@ -13,13 +24,19 @@ async function run() {
   const { context = {} } = github;
   const { issue } = context.payload;
 
-  const { data: issueFromGH } = await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: issue.number,
-    body: "I am so in ACTION!",
+  // const { data: issueFromGH } = await octokit.rest.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: issue.number,
+  //   body: "I am so in ACTION!",
+  // });
+
+  // console.log(issueFromGH);
+
+  const issueMeta = await jiraClient.issues.getCreateIssueMeta({
+    projectIds: [jiraProjectId],
   });
 
-  console.log(issueFromGH);
+  console.log(issueMeta);
 }
 
 run();
